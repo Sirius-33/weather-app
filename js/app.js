52bd4408c830cdf84bb50d16fcabfe667a9f39d3
Daniel H <danielhjermitslev@protonmail.com>
2020-10-05 05:44:00 +0200

    begin date builder

--- js/app.js
+++ js/app.js
@@ -27,4 +27,21 @@ function displayResults(weather) {
     console.log(weather);
     let city = document.querySelector('.location .city');
     city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+    let now = new Date();
+    let date = document.querySelector('.location .date');
+    date.innerText = dateBuilder(now);
+
+}
+
+function dateBuilder (date) {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+    let day = days[date.getDay()];
+    let date= d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
 }
